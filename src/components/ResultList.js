2f189519e9f213ebbf65c4f69b24573d96670bce
aaron weiner <aaronjacobweiner@yahoo.com>
2021-05-31 14:50:03 -0400

    gender organization

--- src/components/ResultList.js
+++ src/components/ResultList.js
@@ -7,14 +7,15 @@ const styles = {
 }
 
 function ResultList(props) {
+
   return (
     <ul className="list-group">
-      {props.results.map(result => (
+      {props.shadowResults.map(result => (
         <li className="table-row" key={result.login.uuid}>
-          <img style={styles.margin} className="col col-1" data-label="Job Id" src={result.picture.medium} />
-          <div className="col col-2" data-label="Customer Name">{result.name.first} {result.name.last}</div>
-          <div className="col col-3" data-label="Amount">{result.gender}</div>
-          <div className="col col-4" data-label="Payment Status">{result.location.city}</div>
+          <img className="col col-1" data-label="Job Id" src={result.picture.medium} />
+          <div className="col col-2 d-flex justify-content-center" data-label="Customer Name">{result.name.first} {result.name.last}</div>
+          <div className="col col-3 d-flex justify-content-center" data-label="Amount">{result.gender}</div>
+          <div className="col col-4 d-flex justify-content-center" data-label="Payment Status">{result.location.city}</div>
         </li>
       ))}
     </ul>
--- src/components/SearchResultContainer.js
+++ src/components/SearchResultContainer.js
@@ -7,11 +7,13 @@ import "./table.css"
 class SearchResultContainer extends Component {
   state = {
     shadowResults: [],
+    shadowB: [],
     results: [],
     search: "",
+    state1: false,
+    state2: false
   };
 
-  // When this component mounts, search the Giphy API for pictures of kittens
   componentDidMount() {
     this.search();
   }
@@ -19,12 +21,82 @@ class SearchResultContainer extends Component {
   search = () => {
     API.search()
       .then(res => {
-        console.log(res.data.results)
         this.setState({ ...this.state, results: res.data.results, shadowResults: res.data.results });
       }
       )
       .catch(err => console.log(err));
   };
+
+
+  handleAction = () => {
+    if (this.state.state1 == false) {
+      var b = this.state.shadowResults.sort((a, b) => {
+        var nameA = a.name.first.toUpperCase();
+        var nameB = b.name.first.toUpperCase();
+        if (nameA < nameB) {
+          return -1;
+        }
+        if (nameA > nameB) {
+          return 1;
+        }
+        return 0;
+      })
+      this.setState({ shadowB: b })
+      this.state.state1 = true;
+      return;
+    }
+    if (this.state.state1 == true) {
+      var b = this.state.shadowResults.sort((a, b) => {
+        var nameA = a.name.first.toUpperCase();
+        var nameB = b.name.first.toUpperCase();
+        if (nameA < nameB) {
+          return 1;
+        }
+        if (nameA > nameB) {
+          return -1;
+        }
+        return 0;
+      })
+      this.setState({ shadowB: b })
+      this.state.state1 = false;
+      return;
+    }
+  }
+
+  genderSort = () => {
+    if (this.state.state2 == false) {
+      var c = this.state.shadowResults.sort((a, b) => {
+        var genderA = a.gender.toUpperCase();
+        var genderB = b.gender.toUpperCase();
+        if (genderA <= genderB) {
+          return -1;
+        }
+        if (genderA >= genderB) {
+          return 1;
+        }
+        return 0;
+      })
+      this.setState({ shadowB: c })
+      this.state.state2 = true;
+      return;
+    }
+    if (this.state.state2 == true) {
+      var c = this.state.shadowResults.sort((a, b) => {
+        var genderA = a.gender.toUpperCase();
+        var genderB = b.gender.toUpperCase();
+        if (genderA <= genderB) {
+          return 1;
+        }
+        if (genderA >= genderB) {
+          return -1;
+        }
+        return 0;
+      })
+      this.setState({ shadowB: c })
+      this.state.state2 = false;
+      return;
+    }
+  }
 
   render() {
     return (
@@ -38,11 +110,13 @@ class SearchResultContainer extends Component {
           <ul className="responsive-table">
             <li className="table-header">
               <button className="col col-1">Picture</button>
-              <button className="col col-2">Name</button>
-              <button className="col col-3">Gender</button>
+              <button onClick={this.handleAction} className="col col-2">Name</button>
+              <button onClick={this.genderSort} className="col col-3">Gender</button>
               <button className="col col-4">Location</button>
             </li>
-            <ResultList results={this.state.results.filter(value => value.name.first.toLowerCase().search(this.state.search.toLowerCase()) !== -1)} />
+            <ResultList
+              results={this.state.results.filter(value => value.name.first.toLowerCase().search(this.state.search.toLowerCase()) !== -1)}
+              shadowResults={this.state.shadowResults.filter(value => value.name.first.toLowerCase().search(this.state.search.toLowerCase()) !== -1)} />
           </ul>
         </div>
       </div>
